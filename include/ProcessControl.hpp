fb4648478ad7a898c862357536f0feaab957f2b7
mpagani <mathiapagani@gmail.com>
2024-06-27 18:50:36 +0200

    create process controll class implementatin for start status and exit commads hhandling

--- include/ProcessControl.hpp
+++ include/ProcessControl.hpp
@@ -1,11 +1,22 @@
 #ifndef PROCESSCONTROL_HPP
 # define PROCESSCONTROL_HPP
 
+#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 #include <map>
+#include <stdexcept>
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <fcntl.h>
+#include <csignal>
 #include <sys/types.h>
-#include <nlohmann/json.hpp>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <algorithm>
 
 using json = nlohmann::json;
 
@@ -14,30 +25,32 @@ class ProcessControl {
     public:
         explicit ProcessControl(const std::string& name, const json& config);
         void start();
-        void stop();
-        void restart();
+        bool isRunning() const;
         std::string getStatus() const;
-    };
+        std::string getName() const;
+        int getStartTime() const { return startTime; }
+
 
     private:
-    std::string name;
-    std::string command;
-    int instances;
-    bool autoStart;
-    std::string autoRestart;
-    int startTime;
-    int stopTime;
-    int restartAttempts;
-    std::string stopSignal;
-    std::vector<int> expectedExitCodes; // dynamic allocation, order doesn't matter
-    std::string workingDirectory;
-    std::string umask;
-    std::string stdoutLog;
-    std::string stderrLog;
-    std::map<std::string, std::string> environmentVariables; // each key unique and quick access to values
-    pid_t pid;
+        std::string name;
+        std::string command;
+        int instances;
+        bool autoStart;
+        std::string autoRestart;
+        int startTime;
+        int stopTime;
+        int restartAttempts;
+        std::string stopSignal;
+        std::vector<int> expectedExitCodes; // dynamic allocation, order doesn't matter
+        std::string workingDirectory;
+        std::string umaskStr;
+        std::string stdoutLog;
+        std::string stderrLog;
+        std::map<std::string, std::string> environmentVariables; // each key unique and quick access to values
+        pid_t pid;
 
-    void parseConfig(const nlohmann::json& config);
-    void setUpEnvironment();
+        void parseConfig(const json& config);
+        void setUpEnvironment();
+    };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
 #include "ConfigParser.hpp"
+#include "ProcessControl.hpp"
+
+void displayUsage() {
+    std::cout << "Usage:" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Commands already implemented:" << std::endl;
+    std::cout << "start <program_name>: Start a program by name. (For programs with start_time = 0, not started at taskmaster launch)" << std::endl;
+    std::cout << "status: Show the status of all programs." << std::endl;
+    std::cout << "exit: Exit the taskmaster." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Commands to be implemented:" << std::endl;
+    std::cout << "stop <program_name>: Stop a running program by name." << std::endl;
+    std::cout << "restart <program_name>: Restart a program by name." << std::endl;
+    std::cout << "reload <program_name>: Reload the configuration of a program without stopping it." << std::endl;
+    std::cout << std::endl;
+}
 
 int main(const int argc, char *argv[]) {
     (void)argc;
@@ -10,13 +26,49 @@ int main(const int argc, char *argv[]) {
         ConfigParser parser(configFilePath);
         json config = parser.getConfig();
 
-        // std::cout << "===== Parsed Configuration Recap =====" << std::endl;
+        std::vector<ProcessControl> processes;
+        for (const auto& item : config.at("programs").items()) {
+                processes.emplace_back(item.key(), item.value());
+            }
 
-        // for (auto it = config.begin(); it != config.end(); ++it) {
-        //     std::cout << "- " << it.key() << ": " << it.value() << std::endl;
-        // }
+        for (auto& process : processes) {
+            if (process.getStartTime() == 1) {
+                process.start();
+            }
+        }
 
-        // std::cout << "======================================" << std::endl;
+        displayUsage();
+
+
+        // Control shell to manage processes
+        std::string command;
+        while (true) {
+            std::cout << "taskmaster> ";
+            std::getline(std::cin, command);
+
+            if (command == "exit") break;
+
+            if (command == "status") {
+                for (auto& process : processes) {
+                    std::cout << process.getName() << ": " << process.getStatus() << std::endl;
+                }
+            } else if (command.rfind("start", 0) == 0) {
+                if (command.length() > 6) {
+                    std::string processName = command.substr(6);
+                    auto it = std::find_if(processes.begin(), processes.end(),
+                        [&processName](ProcessControl& p) { return p.getName() == processName; });
+                    if (it != processes.end()) {
+                        it->start();
+                    } else {
+                        std::cout << "Process not found" << std::endl;
+                    }
+                } else {
+                    std::cout << "Invalid command format. Usage: start <process_name>" << std::endl;
+                }
+            } else {
+                std::cout << "Unknown command" << std::endl;
+            }
+        }
     } catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
         return EXIT_FAILURE;
