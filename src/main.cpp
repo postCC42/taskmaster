f778620003dd8803add864950caea6a37b026b2a
Cpaluszek <paluszek.clement@gmail.com>
2024-07-07 11:28:43 +0200

    use argument for config file path

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <string>
 
 int main(const int argc, char* argv[]) {
-    (void)argc;
-    (void)argv;
-
-    const std::string configFilePath = "./config/multiple_program_config.json";
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file_path>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     try {
-        TaskMaster taskmaster(configFilePath);
+        TaskMaster taskmaster(argv[1]);
     } catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
         return EXIT_FAILURE;
